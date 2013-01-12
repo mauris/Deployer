47986795fef74b773aa14a83ad3b465598035291
Sam-Mauris Yong <mauris@hotmail.sg>
2013-01-12 12:30:02 +0800

    updated bootstrapper file to use composer autoload

--- src/Deployer/Bootstrap.php
+++ src/Deployer/Bootstrap.php
@@ -21,30 +21,23 @@ namespace Deployer;
  * @since 1.0.0
  */
 class Bootstrap {
-    
-    /**
-     * Initializes the bootstrapping process
-     * @since 1.0.0
-     */
-    public static function initialize(){
-        set_include_path(dirname(__DIR__)
-                . PATH_SEPARATOR . get_include_path());
-        spl_autoload_register(function($class) {
-            $class = ltrim($class, '\\');
-            $fileName  = '';
-            $namespace = '';
-            $lastNsPos = strrpos($class, '\\');
-            if($lastNsPos){
-                $namespace = substr($class, 0, $lastNsPos);
-                $class = substr($class, $lastNsPos + 1);
-                $fileName  = str_replace('\\', DIRECTORY_SEPARATOR, $namespace)
-                        . DIRECTORY_SEPARATOR;
-            }
-            $fileName .= str_replace('_', DIRECTORY_SEPARATOR, $class) . '.php';
-            require_once($fileName);
-        });
+
+    private static function acquire($file) {
+        if (is_file($file)) {
+            return include($file);
+        }
     }
-    
+
+    public static function run() {
+        if (!($loader = self::acquire(__DIR__ . '/../../vendor/autoload.php'))) {
+            echo 'You must set up project\'s dependencies first by running the following commands:' . PHP_EOL;
+            echo "    curl -s https://getcomposer.org/installer | php\n";
+            echo "    php composer.phar install\n";
+            exit(1);
+        }
+        return $loader;
+    }
+
 }
 
-Bootstrap::initialize();
+return Bootstrap::run();
