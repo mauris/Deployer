64b626d7755c1fbcf9651ae5df01723f96000014
Sam-Mauris Yong <mauris@hotmail.sg>
2013-01-12 12:49:29 +0800

    updated example

--- example/hook.php
+++ example/hook.php
@@ -1,12 +1,12 @@
 <?php
 include('src/Deployer/Bootstrap.php');
-use Deployer\Drivers\Github\Deployer as Worker;
+use Deployer\Drivers\Github\Deployer;
+use Deployer\Drivers\Github\Payload;
 
 if($_SERVER['REQUEST_METHOD'] == 'POST' && array_key_exists('payload', $_POST)){
     $source = json_decode($_POST['payload'], true);
-    $deployer = new Worker($source, array('target' => '../'));
+    $deployer = new Deployer(Payload::fromCurrent(), array('target' => '../'));
     $deployer->login('username', 'password'); // normally only needed when repository is private
-    $deployer->validate();
     $deployer->deploy();
 
     // write any code here to build the cloned files
