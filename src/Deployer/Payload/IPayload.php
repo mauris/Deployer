c95c8875b87707f067ff41f5dd83fdbdb97ccd96
Sam-Mauris Yong <mauris@hotmail.sg>
2013-01-12 12:08:48 +0800

    payload class and interface implementation

---
+++ src/Deployer/Payload/IPayload.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * Deployer
+ * By Sam-Mauris Yong
+ * 
+ * Released open source under New BSD 3-Clause License.
+ * Copyright (c) Sam-Mauris Yong <sam@mauris.sg>
+ * All rights reserved.
+ */
+
+namespace Deployer\Payload;
+
+/** 
+ * The payload handler interface
+ *
+ * @author Sam-Mauris Yong / mauris@hotmail.sg
+ * @copyright Copyright (c) Sam-Mauris Yong
+ * @license http://www.opensource.org/licenses/bsd-license New BSD License
+ * @package Deployer\Payload
+ * @since 1.0.1
+ */
+interface IPayload {
+    
+    public function __construct($payload);
+    
+    public function name();
+    
+    public function commits();
+    
+}
