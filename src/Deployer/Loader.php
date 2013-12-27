5ac3ffb390d4ef99a68309fd8d778c1201524809
Sam-Mauris Yong <mauris@hotmail.sg>
2013-12-27 15:48:49 +0800

    added loader class

---
+++ src/Deployer/Loader.php
@@ -0,0 +1,12 @@
+<?php /*
+ * Copyright (C) 2013 Sam-Mauris Yong. All rights reserved.
+ * This file is part of the asyncloader.js project, which is released under New BSD 3-Clause license.
+ * See file LICENSE or go to http://opensource.org/licenses/BSD-3-Clause for full license details.
+ */
+
+namespace Deployer;
+
+class Loader
+{
+
+}
