74681dc2e399fdcf2b9526f116910f026e2923a2
Sam-Mauris Yong <mauris@hotmail.sg>
2013-12-27 15:48:39 +0800

    more copyright messages to update

--- src/Deployer/Bootstrap.php
+++ src/Deployer/Bootstrap.php
@@ -1,12 +1,7 @@
-<?php
-
-/**
- * Deployer
- * By Sam-Mauris Yong
- * 
- * Released open source under New BSD 3-Clause License.
- * Copyright (c) Sam-Mauris Yong <sam@mauris.sg>
- * All rights reserved.
+<?php /*
+ * Copyright (C) 2013 Sam-Mauris Yong. All rights reserved.
+ * This file is part of the asyncloader.js project, which is released under New BSD 3-Clause license.
+ * See file LICENSE or go to http://opensource.org/licenses/BSD-3-Clause for full license details.
  */
 
 namespace Deployer;
