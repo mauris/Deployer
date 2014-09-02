94b1d92a3ccd57e945cecb873fbe2aa7a41197f2
Sam-Mauris Yong <mauris@hotmail.sg>
2014-09-02 13:42:46 +0800

    payload to be automatically determined, or $_POST

--- example/hook.php
+++ example/hook.php
@@ -7,4 +7,4 @@ $loader = new Loader();
 $loader->deploy('http://github.com/mauris/Deployer')->to('../deployer');
 $loader->deploy('http://github.com/mauris/example')->to('../example')->with('username', 'User01')->with('password', 'p455w0rd');
 
-$loader->load(Payload::fromCurrent());
+$loader->load();
